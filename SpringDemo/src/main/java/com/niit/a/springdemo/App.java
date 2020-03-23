c872ca864a3c84c037c69e2a0f4ab343426d59d0
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-03-23 11:34:57 +0530

    App.java working

--- SpringDemo/src/main/java/com/niit/a/springdemo/App.java
+++ SpringDemo/src/main/java/com/niit/a/springdemo/App.java
@@ -1,13 +1,16 @@
 package com.niit.a.springdemo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		try {
+			ApplicationContext appCon = new ClassPathXmlApplicationContext("StudentConfig.xml");
+			StudentBean factory = (StudentBean) appCon.getBean("studentbean");
+			factory.displayInfo();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
